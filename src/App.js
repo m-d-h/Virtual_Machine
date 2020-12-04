0c3e4be12d714bc378c341d7aaf25ef9aed655d6
Mohammed Hamid <mohammed.hamid@hashedin.com>
2020-12-04 11:56:23 +0530

    assigment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Tabs from "./components/Tabs"; 
 
@@ -6,15 +6,29 @@ function App() {
   return (
     <div>
       <h1> Virtual Machine Configuration</h1>
-      <h2> Choose Image</h2>
+      
+      <select id = "Select Region">
+          <option value="N/A">N/A</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+      </select>
+
       <Tabs> 
-       <div label="Gator"> 
-         See ya later, <em>Alligator</em>! 
+       <div label="Choose Image"> 
+         See ya later, <em>Fill This</em>! 
        </div> 
-       <div label="Croc"> 
-         After 'while, <em>Crocodile</em>! 
+       <div label="Choose Instance Type"> 
+         After 'while, <em>Fill</em>! 
        </div> 
-       <div label="Sarcosuchus"> 
+       <div label="Choose Storage and Network"> 
+         Nothing to see here, this tab is <em>extinct</em>! 
+       </div> 
+       <div label="Configure Security"> 
+         Nothing to see here, this tab is <em>extinct</em>! 
+       </div> 
+       <div label="Review and Launch"> 
          Nothing to see here, this tab is <em>extinct</em>! 
        </div> 
      </Tabs> 
